ea69c25fc1a66839bb3b8bb3a97e400898ffbe66
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2023-03-18 13:16:20 -0700

    Fix for start up bug

--- anchor/command_line.py
+++ anchor/command_line.py
@@ -1,13 +1,17 @@
+import os
 import sys
 
 from montreal_forced_aligner.command_line.utils import check_databases
-from montreal_forced_aligner.config import GLOBAL_CONFIG
+from montreal_forced_aligner.config import GLOBAL_CONFIG, MFA_PROFILE_VARIABLE
 from montreal_forced_aligner.helper import configure_logger
 
 from anchor.main import Application, MainWindow
 
 
 def main(debug=False):
+    os.environ[MFA_PROFILE_VARIABLE] = "anchor"
+    GLOBAL_CONFIG.current_profile.clean = False
+    GLOBAL_CONFIG.save()
     configure_logger("anchor")
     configure_logger(
         "anchor", GLOBAL_CONFIG.current_profile.temporary_directory.joinpath("anchor.log")
